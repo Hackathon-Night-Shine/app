eb7c69d5bc9683dca944198297b679f7907eeeb1
Poodle Hamood <rimon.aglamaz@gmail.com>
2025-03-05 22:33:40 +0200

    wait

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -6,30 +6,32 @@ import Navbar from './Navbar/Navbar';
 import { theme } from './theme';
 import Home from './Pages/Home/Home';
 import Audience from './Pages/Audience/Audience';
+import { AboutUs } from './Pages/AboutUs';
 
 const CLIENT_ID = '1026926080882-g4hhc1f7kek176kolh4r0bi1v995aq23.apps.googleusercontent.com';
 
 const App = () => {
-    return (
-        <GoogleOAuthProvider clientId={CLIENT_ID}>
-            <ThemeProvider theme={theme}>
-                <CssBaseline />
-                    <BrowserRouter>
-                        <div style={{ width: '100vw', height: '100vh', display: 'flex', flexDirection: 'column', backgroundColor: '#f8f6f0', direction: 'rtl'}}>
-                            <Navbar />
-                            <div style={{ height: '80%'}}>
-                                <Routes>
-                                    <Route path='/audience' element={<Audience/>} />
-                                    <Route path='/contact' element={<Home />} />
-                                    <Route path='/' element={<Home />}/>
-                                    <Route path='*' element={<Home />}/>
-                                </Routes>
-                            </div>
-                        </div>
-                </BrowserRouter>
-            </ThemeProvider>
-        </GoogleOAuthProvider>
-    );
+  return (
+    <GoogleOAuthProvider clientId={CLIENT_ID}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <BrowserRouter>
+          <div style={{ width: '100vw', height: '100vh', display: 'flex', flexDirection: 'column', backgroundColor: '#f8f6f0', direction: 'rtl' }}>
+            <Navbar />
+            <div style={{ height: '80%' }}>
+              <Routes>
+                <Route path='/audience' element={<Audience />} />
+                <Route path='/contact' element={<Home />} />
+                <Route path='/about-us' element={<AboutUs />} />
+                <Route path='/' element={<Home />} />
+                <Route path='*' element={<Home />} />
+              </Routes>
+            </div>
+          </div>
+        </BrowserRouter>
+      </ThemeProvider>
+    </GoogleOAuthProvider>
+  );
 };
 
 export default App;
