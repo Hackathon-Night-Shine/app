124a5e1d92b7f879d540a074f619de057c943bc6
Poodle Hamood <rimon.aglamaz@gmail.com>
2025-03-05 14:43:05 +0200

    main

---
+++ front/src/home/index.tsx
@@ -0,0 +1,24 @@
+export const HomePage = () => {
+	return (
+		<div style={{ display: 'flex', flexDirection: 'column', width: '100%', alignItems: 'center', direction: 'rtl' }}>
+			<img style={{ width: '159px', height: '126px' }}
+				src="https://static.wixstatic.com/media/ded4d3_ea81097b2a1d44b2bc7c1299f0d6acaa~mv2.png/v1/crop/x_0,y_28,w_397,h_278/fill/w_239,h_189,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/logo.png" ></img>
+			<div style={{ width: "100%", height: "546" }}>
+				<img src="https://static.wixstatic.com/media/1ed143_57f11dec29b14e6fbc147fce304e41f3~mv2.jpg/v1/fill/w_1895,h_819,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/1ed143_57f11dec29b14e6fbc147fce304e41f3~mv2.jpg"
+					style={{ height: '100%', width: '100%' }}></img>
+				<img style={{ width: '285px', height: '285px', position: 'absolute', top: '70%', left: '50%', zIndex: 2, transform: 'translate(-50%, -50%)' }}
+					src="https://static.wixstatic.com/media/1ed143_e434f89406ee4a5d8f926498b6513148~mv2.png/v1/fill/w_428,h_428,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/%D7%9C%D7%99%D7%9C%D7%94%20%D7%9B%D7%99%D7%95%D7%9D%20%D7%99%D7%90%D7%99%D7%A8_4x.png"></img>
+			</div>
+
+			<div style={{ width: 650 }}>
+				<h1 style={{ fontSize: '48px', }} >על לילה כיום יאיר</h1>
+				<span >כשבוע מראשית המלחמה יצרנו מרחב ריפוי שנותן מענה מקיף ומקצועי לעיבוד ושחרור של הארועים, על מנת לייצר 'חיסון' שיקטין את הסיכוי להתפתחות תסמינים של פוסט טראומה, ולהעצים את התקווה והאמונה שאפשר לחיות חיים מלאים אחרי ארוע מסוג זה, ואף לצמוח מתוכו.<br />
+					<span ><a href="https://www.laylakeyomyair.org/about-the-retreat" target="_self" >קראו עוד על הריטריטים שלנו&nbsp;</a></span></span>
+
+				<h1 style={{ fontSize: '48px', }} > הארועים הקרובים שלנו</h1>
+				<span >כשבוע מראשית המלחמה יצרנו מרחב ריפוי שנותן מענה מקיף ומקצועי לעיבוד ושחרור של הארועים, על מנת לייצר 'חיסון' שיקטין את הסיכוי להתפתחות תסמינים של פוסט טראומה, ולהעצים את התקווה והאמונה שאפשר לחיות חיים מלאים אחרי ארוע מסוג זה, ואף לצמוח מתוכו.<br />
+				</span>
+			</div>
+		</div>
+	)
+}
