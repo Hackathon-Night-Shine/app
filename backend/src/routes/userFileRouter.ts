dc5872745f6e58d32c78cb79acb8f52c9dd28391
Amit Sheynhaft <214438947@idf.il>
2025-03-06 05:36:39 +0200

    add apis

---
+++ backend/src/routes/userFileRouter.ts
@@ -0,0 +1,31 @@
+import { Router } from "express";
+import { UserFile } from "../entities/UserFile";
+import { dataSource } from "../connection";
+import { UserUserFile } from "../entities/UserUserFile";
+
+const router = Router();
+
+router.get("/userFiles/:userId", async (req, res) => {
+  const userId = parseInt(req.params.userId);
+
+  try {
+    const userRepository = dataSource.getRepository(UserUserFile);
+    const userFile = await userRepository.findOne({
+      where: { userId },
+      relations: ["userFile"],
+      select: ["user_file"],
+    });
+
+    if (!userFile) {
+      res
+        .status(404)
+        .json({
+          message: `User with id ${userId} doesn't have user file data`,
+        });
+      return;
+    }
+    res.json(userFile);
+  } catch (error) {
+    res.status(500).json({ message: "Failed to fetch user file by id", error });
+  }
+});
--- backend/src/routes/userRouter.ts
+++ backend/src/routes/userRouter.ts
@@ -19,7 +19,7 @@ router.get("/users", async (req, res) => {
   }
 });
 
-router.get("/users/:email", async (req, res) => {
+router.get("/users/byEmail/:email", async (req, res) => {
   const email = req.params.email;
 
   try {
@@ -33,6 +33,23 @@ router.get("/users/:email", async (req, res) => {
     res.json(user);
   } catch (error) {
     res.status(500).json({ message: "Failed to fetch user by email", error });
+  }
+});
+
+router.get("/users/byId/:userId", async (req, res) => {
+  const userId = parseInt(req.params.userId);
+
+  try {
+    const userRepository = dataSource.getRepository(User);
+    const user = await userRepository.findOne({ where: { id: userId } });
+
+    if (!user) {
+      res.status(404).json({ message: `User with id ${userId} not found` });
+      return;
+    }
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: "Failed to fetch user by id", error });
   }
 });
 
